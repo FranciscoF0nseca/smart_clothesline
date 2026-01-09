cbac379173ef9d5e55f5e3a540729585bf2664cd
FranciscoF0nseca <pv26039@estgl.ipv.pt>
2026-01-09 17:30:19 +0000

    Fix SECRET_KEY

--- software/estendal_django/DjangoProject/settings.py
+++ software/estendal_django/DjangoProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2jvx9l_uu_9q9tl!cr)hzfm+re+9!w%bli#2s7m)zreen#7^6%'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY não definido nas variáveis de ambiente")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
